3e71f8a91fcb04ff0a25ef7f44799007b06faf82
beingshankar <effective.shankar@gmail.com>
2022-03-14 12:19:11 -0700

    Inject columns data from models

---
+++ src/models/columns.js
@@ -0,0 +1,20 @@
+const columns = [
+  {
+  Header:'Customer',
+  accessor: 'name'
+  },
+  {
+    Header:'Month',
+    accessor: 'month'
+  },
+  {
+    Header: "Transactions Count",
+    accessor: 'numTransactions'
+  },
+  {
+    Header:'Reward Points',
+    accessor: 'points'
+  }
+]
+
+export default columns;
