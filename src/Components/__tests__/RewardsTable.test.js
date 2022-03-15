8fe8bdf811f34a8b95dfb1ce701ea6eeb76140b0
beingshankar <effective.shankar@gmail.com>
2022-03-14 22:57:36 -0700

    Rewards table test file lint fix

--- src/Components/__tests__/RewardsTable.test.js
+++ src/Components/__tests__/RewardsTable.test.js
@@ -1,4 +1,4 @@
-import {render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import RewardsTable from '../RewardsTable';
 import { calculateRewards } from '../../Utils/calculateRewards';
 import React from 'react';
